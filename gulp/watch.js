7b75817bf37ee8227c7f7fb552987c4240413f7f
thijs <Bergenthijs@gmail.com>
2020-02-20 11:48:41 +0100

    removed all

--- gulp/watch.js
+++
@@ -1,6 +0,0 @@
-const { watch, task, series } = require('gulp');
-
-module.exports = task('watch', function(){
-  watch('sass/*.scss', series('sass', 'cssmin')),
-  watch('js/main.js', series('uglify'))
-});
--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const { task, series, src, dest } = require('gulp');
-const sass = require('./gulp/sass');
-const cssmin = require('./gulp/cssmin');
-const uglify = require('./gulp/uglify');
-const watch = require('./gulp/watch');
-const image = require('./gulp/image');
-
-
-task('default', series('watch'));
